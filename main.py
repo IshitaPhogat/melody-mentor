9a16c865390578e69c13ff070ea95d7878c375a3
IshitaPhogat <ishita.phogat0@gmail.com>
2025-06-06 13:29:45 +0530

    u2

--- main.py
+++ main.py
@@ -33,9 +33,21 @@ def init_firebase():
     try:
         # Initialize Firebase Admin
         if not firebase_admin._apps:
-            import json
-            service_account_info = st.secrets["FIREBASE_SERVICE_ACCOUNT_JSON"]
-            service_account_info = json.loads(service_account_info)
+            service_account_info = {
+
+                "type": "service_account",
+                "project_id": st.secrets["FIREBASE_PROJECT_ID"],
+                "private_key_id": st.secrets["FIREBASE_PRIVATE_KEY_ID"],
+                "private_key": st.secrets["FIREBASE_PRIVATE_KEY"],
+                "client_email": st.secrets["FIREBASE_CLIENT_EMAIL"],
+                "client_id": st.secrets["FIREBASE_CLIENT_ID"],
+                "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+                "token_uri": "https://oauth2.googleapis.com/token",
+                "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+                "client_x509_cert_url": st.secrets["FIREBASE_CLIENT_X509_CERT_URL"],
+                "universe_domain": "googleapis.com"
+            }
+
             cred = credentials.Certificate(service_account_info)
             firebase_admin.initialize_app(cred)
 
